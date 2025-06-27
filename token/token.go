1fc44037d2b83cc88647696130c880dad0341f7a
Andre Harper <harperandre277@gmail.com>
2025-06-27 18:52:52 -0400

    completed lexer

--- token/token.go
+++ token/token.go
@@ -1,53 +1,63 @@
-package token 
+package token
 
-
-type TokenType string 
+type TokenType string
 
 type Token struct {
-    Type TokenType 
-    Literal string 
+	Type    TokenType
+	Literal string
 }
 
-const ( 
-    ILLEGAL = "ILLEGAL" 
-    EOF = "EOF"
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foo, x, y
+	INT   = "INT"   // 3221
 
-    // Identifiers + literals 
-    IDENT = "IDENT" // add, foo, x, y 
-    INT = "INT" // 3221
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
+	EQ       = "=="
+	NOT_EQ   = "!="
+	//Delimeters
+	COMMA     = ","
+	SEMICOLON = ";"
 
-    // Operators 
-    ASSIGN = "=" 
-    PLUS = "+" 
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
 
-    //Delimeters 
-    COMMA = "," 
-    SEMICOLON = ";" 
-
-    LPAREN = "(" 
-    RPAREN = ")" 
-    LBRACE = "{" 
-    RBRACE = "}" 
-
-    // keywords 
-    FUNCTION = "FUNCTION" 
-    LET = "LET" 
-
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
 
-
- var keywords = map[string]TokenType{ 
-     "fn": FUNCTION,
-     "let": LET, 
- } 
-
-
- func LookupIdent(ident string) TokenType { 
-     if tok, ok := keywords[ident]; ok { 
-	 return tok 
-     } 
-     return IDENT 
- } 
-
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
