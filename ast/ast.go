e3d812965204ad84a13dfd3642c5d2f2e73deaa1
Andre Harper <harperandre277@gmail.com>
2025-07-09 16:16:38 -0400

    add prefix expression

--- ast/ast.go
+++ ast/ast.go
@@ -29,6 +29,33 @@ type IntegerLiteral struct {
     Value int64 
 }
 
+
+type LetStatement struct {
+	Token token.Token // the token.Let token
+	Name  *Identifier
+	Value Expression
+}
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+type ExpressionStatement struct {
+	Token      token.Token //    first token of the expression
+	Expression Expression
+}
+
+type ReturnStatement struct {
+	Token       token.Token // the return value
+	ReturnValue Expression
+}
+
+type PrefixExpression struct { 
+    Token token.Token 
+    Operator string 
+    Right Expression 
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -37,37 +64,23 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
-type LetStatement struct {
-	Token token.Token // the token.Let token
-	Name  *Identifier
-	Value Expression
-}
+
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
-type Identifier struct {
-	Token token.Token
-	Value string
-}
+
+
 
 func (i *Identifier) expressionNode() {}
 
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
-type ReturnStatement struct {
-	Token       token.Token // the return value
-	ReturnValue Expression
-}
 
 func (rs *ReturnStatement) statementNode() {}
 
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 
-type ExpressionStatement struct {
-	Token      token.Token //    first token of the expression
-	Expression Expression
-}
 
 func (es *ExpressionStatement) statementNode() {}
 
@@ -125,3 +138,20 @@ func (i *Identifier) String() string { return i.Value }
 func (il *IntegerLiteral) expressionNode() {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string { return il.Token.Literal }
+
+
+
+func (pe *PrefixExpression) expressionNode() {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal } 
+
+
+func (pe *PrefixExpression) String() string { 
+    var out bytes.Buffer 
+
+    out.WriteString("(") 
+    out.WriteString(pe.Operator) 
+    out.WriteString(pe.Right.String()) 
+    out.WriteString(")")
+
+    return out.String() 
+}
