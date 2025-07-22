fdaecfe005222a6acd24e653d7d81dd42c73c3be
Andre Harper <harperandre277@gmail.com>
2025-07-21 22:48:56 -0400

    implemented boolean and integer repl functions

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"monkey/lexer"
 	"monkey/parser"
+	"monkey/evaluator"
 )
 
 const MONKEY_FACE = `            __,__
@@ -54,9 +55,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
+		evaluated := evaluator.Eval(program) 
+		if evaluated != nil { 
 
-		io.WriteString(out, program.String())
+		io.WriteString(out, evaluated.Inspect())
 		io.WriteString(out, "\n")
-
+	}
 	}
 }
