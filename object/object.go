427875d4f0ca8a9207f08f4eed29fe5deff0f38f
Andre Harper <harperandre277@gmail.com>
2025-07-23 18:44:48 -0400

    added functions

--- object/object.go
+++ object/object.go
@@ -1,7 +1,10 @@
 package object 
 
 import ( 
-    "fmt" 
+    "fmt"
+    "monkey/ast"
+    "bytes"
+    "strings"
 )
 
 type ObjectType string 
@@ -12,6 +15,7 @@ const (
     BOOLEAN_OBJ = "BOOLEAN"
     NULL_OBJ = "NULL"
     RETURN_VALUE_OBJ = "RETURN_VALUE"
+    FUNCTION_OBJ = "FUNCTION"
 )
 
 type Object interface { 
@@ -32,6 +36,12 @@ type Null struct {}
 
 type ReturnValue struct { 
     Value Object 
+}
+
+type Function struct { 
+    Parameters []*ast.Identifier 
+    Body *ast.BlockStatement
+    Env *Environment
 }
 
 type Error struct { 
@@ -71,6 +81,34 @@ func (rv *ReturnValue) Type() ObjectType {
 
 func (rv *ReturnValue) Inspect() string { 
     return rv.Value.Inspect() 
+}
+
+func (f *Function) Type() ObjectType { 
+    return FUNCTION_OBJ
+}
+ 
+func (f *Function) Inspect() string { 
+
+    var out bytes.Buffer 
+
+    params := []string{} 
+
+    for _, p := range f.Parameters { 
+	params = append(params, p.String())
+    } 
+
+    out.WriteString("fn") 
+    out.WriteString("(") 
+    out.WriteString(strings.Join(params, ", ")) 
+    out.WriteString(") {\n")
+    out.WriteString(f.Body.String())
+    out.WriteString("\n}")
+
+    return out.String()
+
+
+
+
 }
 
 func (e *Error) Type() ObjectType { 
